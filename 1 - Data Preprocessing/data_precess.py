bcacf62a05859cba416b58dd967f9d1f7c5f0e63
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-14 11:49:26 +0300

    Data preprocessing

---
+++ 1 - Data Preprocessing/data_precess.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import sklearn.preprocessing as prepross
+import sklearn.cross_decomposition
+import sklearn.model_selection._split as split
+
+dataset = pd.read_csv('Data.csv')
+
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Handle missing data
+imputer = prepross.Imputer()
+x[:, 1:3] = imputer.fit_transform(x[:, 1:3])
+
+# Encoding categorical data
+label_encoder = prepross.LabelEncoder()
+x[:, 0] = label_encoder.fit_transform(x[:, 0])
+y = label_encoder.fit_transform(y)
+
+# One hot 
+one_hot_encoder_x = prepross.OneHotEncoder(categorical_features = [0])
+x = one_hot_encoder_x.fit_transform(x).toarray()
+
+# Remove first column(categorical data trap)
+x = np.delete(x, 0, axis = 1)
+
+# Split test - train
+x_train, x_test, y_train, y_test = split.train_test_split(x, y, test_size = 0.3)
