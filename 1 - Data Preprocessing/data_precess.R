229eacbd3535471ab0f9f47105bc9a2312f9c700
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-15 12:49:52 +0300

    Encoded logic in functions

--- 1 - Data Preprocessing/data_precess.R
+++ 1 - Data Preprocessing/data_precess.R
@@ -1,11 +1,18 @@
 dataset = read.csv("C:\\DOC\\Workspace\\Machine Learning A-Z Template Folder\\Part 1 - Data Preprocessing\\Data.csv")
 
 # Missing data
-mean_age = mean(dataset$Age, na.rm = TRUE)
-mean_salary = mean(dataset$Salary, na.rm = TRUE)
+fill_missing_data = function(features) {
+  mean = mean(features, na.rm = TRUE)
+  ifelse(is.na(features), mean, features)
+}
+dataset$Age = fill_missing_data(dataset$Age)
+dataset$Salary = fill_missing_data(dataset$Salary)
 
-is_na_age = is.na(dataset$Age)
-is_na_salary = is.na(dataset$Salary)
+# Categorical data
+make_factors = function(features) {
+  levels_features = levels(features)
+  factor(features, levels = levels_features, labels = seq(length(levels_features)))
+}
+dataset$Country = make_factors(dataset$Country)
+dataset$Purchased = make_factors(dataset$Purchased)
 
-dataset$Age = ifelse(is_na_age, mean_age, dataset$Age)
-dataset$Salary = ifelse(is_na_salary, mean_salary, dataset$Salary)
