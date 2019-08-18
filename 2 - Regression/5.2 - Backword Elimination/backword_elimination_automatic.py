e62e7ff1fe3250ee2aef484ee5a97c95422306ff
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-18 20:42:03 +0300

    Automatic backword elimination python

--- 2 - Regression/5.2 - Backword Elimination/backword_elimination_automatic.py
+++ 2 - Regression/5.2 - Backword Elimination/backword_elimination_automatic.py
@@ -8,14 +8,14 @@ import statsmodels.api
 import pandas as pd
 
 # Load data
-data = pd.read_csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/50_Startups.csv')
+data = pd.read_csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/50_Startups - Copy.csv')
 x = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
 
 # Categorical data
 label_encoder = sk.preprocessing.LabelEncoder()
-x[:, -1] = label_encoder.fit_transform(x[:, -1])
-one_hot_encoder = sk.preprocessing.OneHotEncoder(categorical_features = [3])
+x[:, 0] = label_encoder.fit_transform(x[:, 0])
+one_hot_encoder = sk.preprocessing.OneHotEncoder(categorical_features = [0])
 x = one_hot_encoder.fit_transform(x).toarray()
 
 # Dummy variable trap
@@ -28,7 +28,7 @@ x[:, 2:] = scaler.fit_transform(x[:, 2:])
 # Test - train split
 x_train, x_test, y_train, y_test = sk.model_selection._split.train_test_split(x, y, train_size = 0.7)
 
-# Backwords elimination
+# Backwords elimination v1
 def backwords_elimination(x, y):
     model = statsmodels.api.OLS(exog = x, endog = y).fit()
     r_squared = model.rsquared_adj
@@ -43,6 +43,24 @@ def backwords_elimination(x, y):
     else:
         return (model, x)
     
-model, selected_x = backwords_elimination(x_train, y_train)
-model.summary()
+#model, selected_x = backwords_elimination(x_train, y_train)
+#model.summary()
 
+# Backwords elimination v2
+def backwords_elimination_v2(selected_indices):
+    x = x_train[:, selected_indices]
+    model = statsmodels.api.OLS(exog = x, endog = y_train).fit()
+    r_squared = model.rsquared_adj
+    p_values = model.pvalues
+    max_p_value = p_values.max()
+    max_p_value_index = np.where(p_values == max_p_value)[0][0]
+    if max_p_value > 0.05:
+        new_indices = selected_indices.copy()
+        del new_indices[max_p_value_index]    
+        new_model, selected_indices = backwords_elimination_v2(new_indices)
+        new_r_squared = new_model.rsquared_adj
+        return (model, selected_indices) if r_squared > new_r_squared else (new_model, selected_indices)
+    else:
+        return (model, selected_indices)
+    
+model, selected_indices = backwords_elimination_v2(list(range(len(x_train[0]))))
