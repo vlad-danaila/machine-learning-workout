f8a3f45d5491c1d69e5f0a26de7cc6c620637afd
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-19 15:08:32 +0300

    Automatic backword elimination R

--- 2 - Regression/5.2 - Backword Elimination/backword_elimination_automatic.R
+++ 2 - Regression/5.2 - Backword Elimination/backword_elimination_automatic.R
@@ -13,3 +13,16 @@ is_train = sample.split(data$State, SplitRatio = 0.8)
 data_train = subset(data, is_train)
 data_test = subset(data, !is_train)
 
+# Backword elimination
+
+selected_indices = seq(length(data_train))
+selected_data_train = data_train[, selected_indices]
+
+regressor = lm(formula = Profit ~ ., data = selected_data_train)
+summary = summary(regressor)
+p_values = (summary$coefficients)[, 4]
+adj_r_squared = summary$adj.r.squared
+
+max_p_value_index = which(p_values == max(p_values))
+new_selected_indeices = selected_indices[-max_p_value_index]
+
