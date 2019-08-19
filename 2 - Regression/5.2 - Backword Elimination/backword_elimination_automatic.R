2434b22a08cfc5d226ebbe362fe26c7ab2e978ce
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-19 16:08:42 +0300

    Automatic backwords elimination in R

--- 2 - Regression/5.2 - Backword Elimination/backword_elimination_automatic.R
+++ 2 - Regression/5.2 - Backword Elimination/backword_elimination_automatic.R
@@ -13,16 +13,30 @@ is_train = sample.split(data$State, SplitRatio = 0.8)
 data_train = subset(data, is_train)
 data_test = subset(data, !is_train)
 
-# Backword elimination
-
+# Backword elimination - Not working properly
+# The problem is with factors being unpacked in the model but not whem selecting the data
+# Wold need to create a one hot encoding as in python
+"browser()
 selected_indices = seq(length(data_train))
-selected_data_train = data_train[, selected_indices]
-
-regressor = lm(formula = Profit ~ ., data = selected_data_train)
-summary = summary(regressor)
-p_values = (summary$coefficients)[, 4]
-adj_r_squared = summary$adj.r.squared
-
-max_p_value_index = which(p_values == max(p_values))
-new_selected_indeices = selected_indices[-max_p_value_index]
+for (i in seq(length(data_train))) {
+  selected_data_train = data_train[, selected_indices]
+  regressor = lm(formula = Profit ~ ., data = selected_data_train)
+  summary = summary(regressor)
+  p_values = (summary$coefficients)[, 4]
+  adj_r_squared = summary$adj.r.squared
+  max_p_value = max(p_values)
+  if (max_p_value > 0.05) {
+    max_p_value_index = which(p_values == max_p_value)
+    new_selected_indeices = selected_indices[-max_p_value_index]
+    newModel = lm(formula = Profit ~ ., data = data_train[, new_selected_indeices])
+    new_adj_r_squared = summary(newModel)$adj.r.squared
+    if (adj_r_squared < new_adj_r_squared) {
+      regressor = newModel
+      selected_indices = new_selected_indeices
+    } 
+  } else {  
+    break
+  }
+}
+"
 
