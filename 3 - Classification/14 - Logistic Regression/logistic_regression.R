06910a2c2f3bcfd7702d542150c517ae7c4b8bf0
Vlad Danaila <vlad.danaila@yahoo.com>
2019-08-22 02:29:55 +0300

    Logistic regression R

--- 3 - Classification/14 - Logistic Regression/logistic_regression.R
+++ 3 - Classification/14 - Logistic Regression/logistic_regression.R
@@ -3,7 +3,7 @@ data = data[3:5]
 
 # Make factors for y
 purchased_levels = unique(data$Purchased)
-data$Purchased = factor(data$Purchased, levels = purchased_levels, labels = seq(length(purchased_levels)))
+data$Purchased = factor(data$Purchased, levels = purchased_levels)
 
 # Scaling
 data[1:2] = scale(data[1:2])
@@ -25,4 +25,17 @@ accuracy =  sum(diag(cm)) / sum(cm)
 print(accuracy)
 
 # Plot
-
+#install.packages('ElemStatLearn')
+library(ElemStatLearn)
+grid_x_1 = seq(min(data_test[1] - 1), max(data_test[1] + 1), by = 0.03)
+grid_x_2 = seq(min(data_test[2] - 1), max(data_test[2] + 1), by = 0.03)
+grid = expand.grid(grid_x_1, grid_x_2)
+colnames(grid) = colnames(data_test)[-3]
+grid_pred = predict(classifier, grid, type = 'response')
+grid_pred = ifelse(grid_pred > 0.5, 1, 0)
+plot(data_test[, -3],
+     main = 'Logistic Regression',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(grid_x_1), ylim = range(grid_x_2))
+points(grid, pch = 5, col = ifelse(grid_pred == 1, 'springgreen3', 'tomato'))
+points(data_test, pch = 21, bg = ifelse(data_test[, 3] == 1, 'green4', 'red3'))
