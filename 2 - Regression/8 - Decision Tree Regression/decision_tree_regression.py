865ed1a596180e4992991e4d060b505f159b1f2f
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-21 13:52:03 +0300

    Random forest python

--- 2 - Regression/8 - Decision Tree Regression/decision_tree_regression.py
+++ 2 - Regression/8 - Decision Tree Regression/decision_tree_regression.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 import sklearn as sk
 import sklearn.tree
+import sklearn.ensemble
 import sklearn.preprocessing
 import matplotlib.pyplot as plt
 
@@ -17,17 +18,23 @@ x, y = data.iloc[:, 1].values, data.iloc[:, -1].values
 x, y = np.expand_dims(x, axis = 1), np.expand_dims(y, axis = 1)
 
 # Decision tree
-regressor = sk.tree.DecisionTreeRegressor() 
-regressor.fit(x, y)
+regressor_tree = sk.tree.DecisionTreeRegressor() 
+regressor_tree.fit(x, y)
+
+# Random forest
+regressor_forest = sk.ensemble.RandomForestRegressor()
+regressor_forest.fit(x, y)
 
 # Prediction
 x_grid = np.arange(x.min(), x.max(), step = 0.0001)
 x_grid = x_grid.reshape((x_grid.shape[0], 1))
-pred = regressor.predict(x_grid)
+pred_tree = regressor_tree.predict(x_grid)
+pred_forst = regressor_forest.predict(x_grid)
 
 # Plot
-plt.scatter(x, y)
-plt.plot(x_grid, pred)
+plt.scatter(x, y, color = 'red')
+plt.plot(x_grid, pred_tree, color = 'yellow')
+plt.plot(x_grid, pred_forst, color = 'orange')
 plt.title('Decision tree regression')
 plt.xlabel('Career level')
 plt.ylabel('salary')
