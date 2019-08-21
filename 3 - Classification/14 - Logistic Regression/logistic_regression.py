17bb469a4442a8964af2e3856185876682c34f96
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-21 18:01:08 +0300

    Logistic regression python + plot

--- 3 - Classification/14 - Logistic Regression/logistic_regression.py
+++ 3 - Classification/14 - Logistic Regression/logistic_regression.py
@@ -6,6 +6,8 @@ import pandas as pd
 import sklearn.preprocessing
 import sklearn.model_selection._split
 import sklearn.linear_model
+import matplotlib.pyplot as plt
+import matplotlib.colors
 
 # Load data
 data = pd.read_csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 14 - Logistic Regression/Social_Network_Ads.csv')
@@ -32,4 +34,13 @@ accuracy = np.sum(np.diag(cm)) / np.sum(cm)
 print('Accuracy is', accuracy)
 
 # Plot
+x_0_coord = np.arange(x_train[:, 0].min(), x_train[:, 0].max(), 0.01)
+x_1_coord = np.arange(x_train[:, 1].min(), x_train[:, 1].max(), 0.01)
+grid_x_0, grid_x_1 = np.meshgrid(x_0_coord, x_1_coord)
+grid_x_0_1 = np.array([grid_x_0.ravel(), grid_x_1.ravel()]).T
+grid_pred = classifier.predict(grid_x_0_1).reshape(grid_x_0.shape)
+plt.contourf(grid_x_0, grid_x_1, grid_pred, cmap = matplotlib.colors.ListedColormap(('red', 'green')))
+plt.scatter(x_test[y_test == 0, 0], x_test[y_test == 0, 1], color = 'darkred')
+plt.scatter(x_test[y_test == 1, 0], x_test[y_test == 1, 1], color = 'darkgreen')
+plt.show()
 
