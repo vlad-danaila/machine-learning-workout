492409a256cb41ee39df4c12fb3fe7092d386797
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-21 13:05:44 +0300

    SVR R

--- 2 - Regression/7 - SVR/svr.R
+++ 2 - Regression/7 - SVR/svr.R
@@ -1,9 +1,31 @@
+#install.packages('e1071')
+library(e1071)
+library(ggplot2)
+
+# Data loading
 data = read.csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/Position_Salaries.csv')
 data = data[-1]
 
 # Feature scalling
-data = scale(data)
+data_scaled = scale(data)
 
 # SVR
-install.packages('e1071')
-library(e1071)
+regressor = svm(formula = Salary ~ ., data = data_scaled)
+
+# Make predictions
+x = data.frame(Level = seq(min(data$Level), max(data$Level), 0.1))
+x_scaled = scale(x)
+pred_scaled = predict(regressor, newdata = x_scaled)
+
+# Unscale predictions
+mean = attr(data_scaled,"scaled:center")['Salary']
+variance = attr(data_scaled,"scaled:scale")['Salary']
+pred = pred_scaled * variance + mean 
+
+# Plot
+ggplot() + 
+  geom_point(aes(x = data$Level, y = data$Salary), color = 'red') + 
+  geom_line(aes(x = x$Level, y = pred), color = 'blue') + 
+  ggtitle('SVR') + 
+  xlab('Career level') + 
+  ylab('Salary')
