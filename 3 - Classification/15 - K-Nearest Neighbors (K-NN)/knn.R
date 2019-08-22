2f13143251b31f9fb0a500f764013aa7475d4809
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-22 16:10:55 +0300

    Wrong plotting R

--- 3 - Classification/15 - K-Nearest Neighbors (K-NN)/knn.R
+++ 3 - Classification/15 - K-Nearest Neighbors (K-NN)/knn.R
@@ -34,3 +34,27 @@ pred_test = knn(
 cm = table(data_test$Purchased, pred_test)
 accuracy = sum(diag(cm)) / sum(cm)
 cat('Accuracy is', accuracy)
+
+# Make grid for plotting
+make_grid = function(v, steps = 100) {
+  v_limit_min = min(v) - 1
+  v_limit_max = max(v) + 1
+  seq(v_limit_min, v_limit_max, by = (v_limit_max - v_limit_min) / steps)
+}
+grid_x_0 = make_grid(data_test$Age)
+grid_x_1 = make_grid(data_test$EstimatedSalary)
+grid_x_0_1 = expand.grid(grid_x_0, grid_x_1)
+colnames(grid_x_0_1) = colnames(data[-3])
+grid_x_0_1_scaled = scale_matrix(grid_x_0_1, means, variances)
+grid_pred = knn(
+  train = data_train_scaled[-3], test = grid_x_0_1_scaled, cl = data_train$Purchased, k = 5)
+
+# Display plot
+plot(
+  data_test$Age, 
+  data_test$EstimatedSalary,
+  main = 'KNN',
+  xlab = 'Age', 
+  ylab = 'Sallary')
+points(grid_x_0_1$Age, grid_x_0_1$EstimatedSalary, pch = 16, col = ifelse(grid_pred == 1, 'red', 'darkgreen'))
+#points(data_test$Age, data_test$EstimatedSalary, pch = 16, col = ifelse(data_test$Purchased, 'red', 'darkgreen'))
