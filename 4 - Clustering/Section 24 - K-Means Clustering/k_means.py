48e80ae899042cc752b1c1ef12b927d683e1565d
Vlad Danaila <vlad.danaila@yahoo.com>
2019-08-23 22:12:50 +0300

    KNN

--- 4 - Clustering/Section 24 - K-Means Clustering/k_means.py
+++ 4 - Clustering/Section 24 - K-Means Clustering/k_means.py
@@ -11,13 +11,27 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('C:/DOC\Workspace/Machine Learning A-Z Template Folder/Part 4 - Clustering/Section 24 - K-Means Clustering/Mall_customers.csv')
 x = data.iloc[:, [3,4]].values
 
+fig, sublot = plt.subplots(2)
+fig.suptitle('K-Means')
+
+# Within cluster sum of squares (WCSS)
+wcss = []
+max_nr_clusters = 20
+for nr_clusters in range(2, max_nr_clusters):
+    k_means = sk.cluster.KMeans(n_clusters = nr_clusters)    
+    k_means.fit(x)
+    wcss.append(k_means.inertia_)
+    
+sublot[0].plot(range(2, max_nr_clusters), wcss)
+sublot[0].set_title('Within cluster sum of squares')
+
 # Cluster
-nr_clusters = 4
+nr_clusters = 5
 k_means = sk.cluster.KMeans(n_clusters = nr_clusters)
 x_clustered = k_means.fit_predict(x)
 
 # Plot clusters
-for _class, color in enumerate(('red', 'green', 'blue', 'yellow')):
+for _class, color in enumerate(('red', 'green', 'blue', 'yellow', 'orange')):
     is_class = x_clustered == _class
-    plt.scatter(x[is_class, 0], x[is_class, 1], color = color)
+    sublot[1].scatter(x[is_class, 0], x[is_class, 1], color = color)
 plt.show()
