75468718803dcd30d81ff46dc67be4f649e236cc
Vlad Danaila <vlad.danaila@yahoo.com>
2019-08-25 15:35:02 +0300

    Hierachical clustering by distance treshold.

--- 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.py
+++ 4 - Clustering/Section 25 - Hierarchical Clustering/hierarchical_clustering.py
@@ -19,15 +19,15 @@ plt.xlabel('Clusters')
 plt.show()
 
 # Fit model
-n_clusters = 5
-clustering = sklearn.cluster.hierarchical.AgglomerativeClustering(n_clusters = n_clusters)
+clustering = sklearn.cluster.hierarchical.AgglomerativeClustering(
+        distance_threshold = 100, n_clusters = None)
 x_clustered = clustering.fit_predict(x)
 
 # Plot clusters
 colors = ('red', 'green', 'blue', 'yellow', 'orange')
 get_color = lambda i: colors[i % len(colors)]
 
-for i in range(n_clusters):
+for i in range(clustering.n_clusters_):
     plt.scatter(x[x_clustered == i, 0], x[x_clustered == i, 1], color = get_color(i))
     # Plot centroids
     plt.scatter(np.mean(x[x_clustered == i, 0]), np.mean(x[x_clustered == i, 1]), color = 'black')
@@ -35,3 +35,4 @@ plt.title('Clusters')
 plt.xlabel('Income')
 plt.ylabel('Spending score')
 plt.show()
+
