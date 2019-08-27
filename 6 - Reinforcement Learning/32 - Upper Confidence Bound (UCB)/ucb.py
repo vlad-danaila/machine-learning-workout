52c67c15e581a70c456661fd68cd01f9dcecaab8
Vlad Danaila <vlad.danaila@yahoo.com>
2019-08-27 21:20:18 +0300

    Upper confidence bound

--- 6 - Reinforcement Learning/32 - Upper Confidence Bound (UCB)/ucb.py
+++ 6 - Reinforcement Learning/32 - Upper Confidence Bound (UCB)/ucb.py
@@ -3,7 +3,7 @@
 import numpy as np, math, pandas as pd, matplotlib.pyplot as plt
 
 x = list(range(10, 1000))
-y = [ math.sqrt(3/2 * math.log(_x) / math.ceil(_x / 200))  for _x in x]
+y = [math.sqrt(3/2 * math.log(_x) / math.ceil(_x / 200))  for _x in x]
 
 plt.plot(x, y)
 plt.title('Bound fluctuation visualisation')
@@ -12,10 +12,25 @@ plt.ylabel('Bound')
 plt.show() 
 
 data = pd.read_csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/Ads_CTR_Optimisation.csv')
-ads = data.iloc[:].values
+data = data.iloc[:].values
 
-N = len(ads[0])
-counts, rewards, upper_bounds = np.zeros(N), np.zeros(N), np.full((N), np.Infinity)
+N = len(data[0])
+counts, rewards, mean_rewards = np.zeros(N), np.zeros(N), np.zeros(N)
+upper_bounds = np.full((N), np.Infinity)
+selected_ads = []
 
-for ad in ads:
-    print(ad)
+for i, clicked_ads in enumerate(data):
+    selected_ad = np.argmax(upper_bounds)
+    selected_ads.append(selected_ad)
+    counts[selected_ad] += 1
+    rewards[selected_ad] += clicked_ads[selected_ad]
+    mean_rewards[selected_ad] = rewards[selected_ad] / counts[selected_ad]
+    # update bounds
+    confidence_bound = math.sqrt(3/2 * math.log(i + 1) / counts[selected_ad])
+    upper_bounds[selected_ad] = mean_rewards[selected_ad] + confidence_bound
+    
+plt.hist(selected_ads)
+plt.title('Selected ads')
+plt.xlabel('Ads identifiers')
+plt.ylabel('Nr. of selections')
+plt.show()
