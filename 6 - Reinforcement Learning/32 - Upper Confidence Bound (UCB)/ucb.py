e7948c1ccb1f4186b055d1e0ddd10f45364b6c10
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-27 18:23:44 +0300

    UCB

---
+++ 6 - Reinforcement Learning/32 - Upper Confidence Bound (UCB)/ucb.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np, math, pandas as pd, matplotlib.pyplot as plt
+
+x = list(range(10, 1000))
+y = [ math.sqrt(3/2 * math.log(_x) / math.ceil(_x / 200))  for _x in x]
+
+plt.plot(x, y)
+plt.title('Bound fluctuation visualisation')
+plt.xlabel('Nr. of rounds')
+plt.ylabel('Bound')
+plt.show() 
+
+data = pd.read_csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/Ads_CTR_Optimisation.csv')
+ads = data.iloc[:].values
+
+N = len(ads[0])
+counts, rewards = np.zeros(N), np.zeros(N)
+
+for ad in ads:
+    print(ad)
