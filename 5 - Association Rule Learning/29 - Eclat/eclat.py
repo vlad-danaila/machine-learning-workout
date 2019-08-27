acba0429dd76413501102997f0deb27ea0331c50
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-27 17:14:00 +0300

    Refactor using collections.defaultdict

--- 5 - Association Rule Learning/29 - Eclat/eclat.py
+++ 5 - Association Rule Learning/29 - Eclat/eclat.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import itertools
-import pandas as pd
-import math
+import collections, itertools, pandas as pd
 
 MIN_SUPPORT = 0.001
 MIN_LENGTH = 3
@@ -12,22 +10,18 @@ DATA_PATH = 'C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 5 - Asso
 data = pd.read_csv(DATA_PATH, header = None)
 
 def get_combinations(v):
-    combinations = []
     for i in range(MIN_LENGTH, min(MAX_LENGTH + 1, max(MIN_LENGTH, len(v) + 1))):
         for x in itertools.combinations(v, i):
             yield frozenset(x)
 
-products_support = {}
 total = len(data)
+products_support = collections.defaultdict(lambda: 0)
 
 for i, transaction in data.iterrows():
     products = [product for product in transaction if type(product) is str]
     for prod_combination in get_combinations(products):
-        if prod_combination in products_support:
-            products_support[prod_combination] += 1 / total
-        else:
-            products_support[prod_combination] = 1 / total
-    
+        products_support[prod_combination] += 1 / total    
+        
 products_support_list = [ (p, support) for p, support in products_support.items() if support > MIN_SUPPORT ]
 products_support_list.sort(key = lambda elem: elem[1], reverse = True)
 
