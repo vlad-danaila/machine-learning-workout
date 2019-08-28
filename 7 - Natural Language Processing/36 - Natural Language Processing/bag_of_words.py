8b65d3a5ccb2541b1b81f58649b829c1add54e0b
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-28 18:37:07 +0300

    Bag of words

---
+++ 7 - Natural Language Processing/36 - Natural Language Processing/bag_of_words.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.model_selection._split
+import sklearn.naive_bayes
+import nltk
+import nltk.stem.porter
+import nltk.corpus
+import re
+
+nltk.download('stopwords')
+
+DATA_PATH = 'C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Restaurant_Reviews.tsv'
+data = pd.read_csv(DATA_PATH, sep = '\t')
+x, y = data.iloc[:, 0].values, data.iloc[:, 1].values
+
+stopwords = nltk.corpus.stopwords.words('english')
+stemmer = nltk.stem.porter.PorterStemmer()
+
+def transform_review_text(text):
+    text = text.lower()
+    text = re.sub('[^a-z ]', '', text)
+    text_array = [stemmer.stem(word) for word in text.split() if word not in stopwords]
+    return ' '.join(text_array)
+    
+x = [transform_review_text(review) for review in x]
+
