ce8b5ae1da46b7abe9b106c52bdab0f10069ad40
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-08-29 17:21:52 +0300

    Neural net

--- 8 - Deep Learning/39 - Artificial Neural Networks (ANN)/nn.py
+++ 8 - Deep Learning/39 - Artificial Neural Networks (ANN)/nn.py
@@ -14,7 +14,7 @@ data = pd.read_csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 8
 
 x, y = data.iloc[:, 3:-1].values, data.iloc[:, -1].values
 
-x_train, x_test, y_train, y_test = sk.model_selection._split.train_test_split(x, y, train_size = .8)
+x_train, x_test, y_train, y_test = sk.model_selection._split.train_test_split(x, y, train_size = .5)
 
 # Prepare scaler
 scaled_columns = [0, 3, 4, 5, 6, 9]
@@ -33,5 +33,26 @@ def process_data(x):
     x = np.hstack((x[:, non_one_hot_columns], one_hot_data))
     return x
 
-x_train = process_data(x_train)
-x_test = process_data(x_test)
+x_train, x_test = process_data(x_train), process_data(x_test)
+
+# Create neural network
+model = ks.models.Sequential((
+    ks.layers.Dense(13, activation = 'relu', input_shape = (13,)),
+    ks.layers.Dense(6, activation = 'relu'),  
+    ks.layers.Dense(3, activation = 'relu'),
+    ks.layers.Dense(1, activation = 'sigmoid'),        
+))
+
+# Train model
+model.compile('adam', loss = ks.losses.binary_crossentropy, metrics = ['accuracy'])
+model.fit(x_train, y_train, batch_size = 32, epochs = 3, validation_split = 0.3)
+
+# Make predictions
+y_pred = model.predict(x_test)
+y_pred = y_pred > 0.5
+
+confusion_matrix = sk.metrics.confusion_matrix(y_test, y_pred)
+print('Confusion matrix', confusion_matrix, sep = '\n')
+
+accuracy = np.sum(np.diag(confusion_matrix)) / np.sum(confusion_matrix)
+print('Accuracy', accuracy, sep = '\n')
