7070d234c8cae870d26258d65bea792f89a4b250
Vlad Danaila <vlad.danaila@yahoo.com>
2019-09-01 15:13:14 +0300

    LDA manual version

--- 9 - Dimensionality Reduction/44 - Linear Discriminant Analysis (LDA)/lda_manual.py
+++ 9 - Dimensionality Reduction/44 - Linear Discriminant Analysis (LDA)/lda_manual.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import sklearn as sk
 import sklearn.preprocessing
+import matplotlib.pyplot as plt
 
 # Loading data
 iris_dataset = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -28,9 +29,25 @@ for x_cls in x_class:
     # Mean of features per class
     mean_cls = np.mean(x_cls, axis=0) # 4 
     mean_diff = mean_cls - mean_all # 4 - 4 = 4
-    mean_diff = mean_diff.reshape(4, 1) # Make a column vector
+    mean_diff = mean_diff.reshape(4, 1) # Make a column vector 4 x 1
     diff = x_cls - mean_cls # N x 4 - 4 = N x 4
     within_class_scatter += diff.T.dot(diff) # 4 x N dot N x 4 = 4 x 4
-    between_class_scatter += len(x_cls) * mean_diff.dot(mean_diff.T)
+    between_class_scatter += len(x_cls) * mean_diff.dot(mean_diff.T) # 4 x 1 dot 1 x 4 = 4 x 4 
     
-    
+# Create final matrix and perform eigen decomposition
+# We want to minimize within class variance and maximize between class variance
+scatter_div = np.linalg.inv(within_class_scatter).dot(between_class_scatter) # 4 x 4
+eigen_vals, eigen_vect = np.linalg.eig(scatter_div) # 4, 4 x 4
+
+# Get only the first 2 components
+lda_matrix = eigen_vect[:, :2]
+
+# Transform x
+x_transformed = x.dot(lda_matrix)
+
+# Plot
+colors = 'red', 'green', 'blue'
+for i in range(3):
+    plt.scatter(x_transformed[y == i][:, 0], x_transformed[y == i][:, 1], color = colors[i])
+plt.title('LDA')
+plt.show()
