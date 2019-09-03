e8e8541a33933215ea724c57dd939cb2c69e386f
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-09-03 15:57:16 +0300

    boosting xgboost

---
+++ 11 - Boosting/49 - XGBoost/xg_boost.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import xgboost
+import numpy as np
+import sklearn as sk
+import pandas as pd
+import sklearn.preprocessing
+import sklearn.model_selection
+import sklearn.metrics
+import matplotlib.pyplot
+import matplotlib.colors
+
+data = pd.read_csv('C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 10 - Model Selection & Boosting/Section 49 - XGBoost/Churn_Modelling.csv')
+x, y = data.iloc[:, 3:-1].values, data.iloc[:, -1].values
+
+# Train test split
+x_train, x_test, y_train, y_test = sk.model_selection.train_test_split(x, y, train_size = 0.8)
+
+# Scaling
+scaled_columns = [0, 3, 4, 5, 6, 9]
+scaler = sk.preprocessing.StandardScaler()
+scaler.fit(x_train[:, scaled_columns])
+
+# Categorical data
+one_hot_columns = [1, 2]
+non_one_hot_columns = list(set(range(len(x[0]))) - set(one_hot_columns)) 
+one_hot_encoders = [sk.preprocessing.OneHotEncoder(sparse = False) for i in one_hot_columns] 
+for i in range(len(one_hot_columns)):
+    one_hot_encoders[i].fit(x_train[:, [one_hot_columns[i]]])
+
+def prepare_data(x):
+    x[:, scaled_columns] = scaler.transform(x[:, scaled_columns])
+    for i in range(len(one_hot_columns)):
+        one_hot = one_hot_encoders[i].transform(x[:, [one_hot_columns[i]]])
+        one_hot = np.delete(one_hot, 0, axis = 1)
+        x = np.hstack((x, one_hot))
+    x = np.delete(x, one_hot_columns, axis = 1)
+    x = x.astype(np.float64)
+    return x
+
+x_train, x_test = prepare_data(x_train), prepare_data(x_test)
+
