b2442bcb95379844d28b78567e7ef125e3ae576a
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-09-03 16:13:56 +0300

    XGBoost

--- 11 - Boosting/49 - XGBoost/xg_boost.py
+++ 11 - Boosting/49 - XGBoost/xg_boost.py
@@ -23,11 +23,11 @@ scaler.fit(x_train[:, scaled_columns])
 
 # Categorical data
 one_hot_columns = [1, 2]
-non_one_hot_columns = list(set(range(len(x[0]))) - set(one_hot_columns)) 
 one_hot_encoders = [sk.preprocessing.OneHotEncoder(sparse = False) for i in one_hot_columns] 
 for i in range(len(one_hot_columns)):
     one_hot_encoders[i].fit(x_train[:, [one_hot_columns[i]]])
 
+# Data preparation
 def prepare_data(x):
     x[:, scaled_columns] = scaler.transform(x[:, scaled_columns])
     for i in range(len(one_hot_columns)):
@@ -40,3 +40,10 @@ def prepare_data(x):
 
 x_train, x_test = prepare_data(x_train), prepare_data(x_test)
 
+# Define classifier
+model = xgboost.XGBClassifier()
+
+# Asses accuracy
+f1_scores = sk.model_selection.cross_val_score(model, x_train, y_train, scoring = 'f1', cv = 10, n_jobs = -1)
+print('F1 mean is', f1_scores.mean())
+print('F1 standard variation is', f1_scores.std())
