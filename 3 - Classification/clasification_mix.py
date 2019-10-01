595a79f102112b091e7b21e947db3238e81ab759
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-10-01 14:59:29 +0300

    Classification mix + pca

--- 3 - Classification/clasification_mix.py
+++ 3 - Classification/clasification_mix.py
@@ -8,6 +8,11 @@ import sklearn.model_selection
 import sklearn.linear_model
 import sklearn.metrics
 import sklearn.decomposition
+import sklearn.neighbors
+import sklearn.tree
+import sklearn.ensemble
+import sklearn.naive_bayes
+import sklearn.svm
 import pandas as pd
 
 DATASET_PATH = 'C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 3 - Classification/Section 16 - Support Vector Machine (SVM)/Social_Network_Ads.csv'
@@ -27,16 +32,19 @@ x_test = scaler.transform(x_test)
 
 # Define models
 models = {}
-
-logistic = sk.linear_model.LogisticRegression()
-models['logistic'] = logistic
-
+models['logistic'] = sk.linear_model.LogisticRegression()
+models['knn'] = sk.neighbors.KNeighborsClassifier()
+models['decision tree'] = sk.tree.DecisionTreeClassifier()
+models['random forest'] = sk.ensemble.RandomForestClassifier()
+models['naive'] = sk.naive_bayes.GaussianNB()
+models['svm'] = sk.svm.SVC()
 
 # PCA is aplied here only for visualisation
 pca = sk.decomposition.PCA(2)
 x_train_pca = pca.fit_transform(x_train)
 x_test_pca = pca.transform(x_test)
 
+# Prepare decision boundry plot visualization
 def segmented(x, steps = 1000):
     min, max = np.min(x), np.max(x)
     padd = (max - min) / 5
@@ -47,18 +55,18 @@ x0_mesh, x1_mesh = np.meshgrid(x0, x1)
 x0_1 = np.c_[x0_mesh.ravel(), x1_mesh.ravel()]
 x_grid = pca.inverse_transform(x0_1)
 
-logistic.fit(x_train, y_train)
-y_pred = logistic.predict(x_test)
-conf_matrix = sk.metrics.confusion_matrix(y_test, y_pred)
-accuracy = np.sum(np.diag(conf_matrix)) / np.sum(conf_matrix)
-y_grid = logistic.predict(x_grid).reshape(x0_mesh.shape)
+def plot_decision_boundary(y_grid, title, accuracy):
+    plt.contourf(x0, x1, y_grid)
+    plt.scatter(x_test_pca[y_test == 0, 0], x_test_pca[y_test == 0, 1])
+    plt.scatter(x_test_pca[y_test == 1, 0], x_test_pca[y_test == 1, 1])
+    plt.title('{} (accuracy {})'.format(title, accuracy))
+    plt.show()
 
-plt.contourf(x0, x1, y_grid)
-plt.scatter(x_test_pca[y_test == 0, 0], x_test_pca[y_test == 0, 1])
-plt.scatter(x_test_pca[y_test == 1, 0], x_test_pca[y_test == 1, 1])
-
-'''
-# Train & evaluate models
+# Train, evaluate, visalize
 for model_name, model in models.items():
-    model.fit(x_train)
-    '''
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    conf_matrix = sk.metrics.confusion_matrix(y_test, y_pred)
+    accuracy = np.sum(np.diag(conf_matrix)) / np.sum(conf_matrix)
+    y_grid = model.predict(x_grid).reshape(x0_mesh.shape)
+    plot_decision_boundary(y_grid, model_name, accuracy)
