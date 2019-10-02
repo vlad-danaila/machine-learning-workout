f8b7e3099c89eabb6830bcd8ba92f7547e8f57de
VLAD RARES DANAILA <vlad.danaila@ro.ibm.com>
2019-10-02 17:11:45 +0300

    Clustering combined

--- 4 - Clustering/clustering_all.py
+++ 4 - Clustering/clustering_all.py
@@ -21,30 +21,19 @@ def get_color(i):
 def cluster_variance(cluster):
     return np.linalg.norm(cluster - cluster.mean(axis = 0), axis = 1).sum()
 
-def cluster_kmeans(nb_clusters):
-    kmeans = sk.cluster.KMeans(nb_clusters)
-    clustered = kmeans.fit_predict(x)
-    for i in range(nb_clusters):
-        plt.scatter(x[clustered == i, 0], x[clustered == i, 1], color = get_color(i))
-    plt.title('KMeans ({} clusters, {} variance)'.format(nb_clusters, kmeans.inertia_))
-    plt.show()
-    return kmeans.inertia_
-
-#for nb_clusters in range(1, 11):
- #   cluster_kmeans(nb_clusters)
-
-for nb_clusters in range(1, 11):
-    hierarchical = sk.cluster.AgglomerativeClustering(nb_clusters)
+def cluster_data(nb_clusters, cluster_model, model_name):
+    hierarchical = cluster_model(nb_clusters)
     clustered = hierarchical.fit_predict(x)
     total_variance = 0
     for i in range(nb_clusters):
         cluster = x[clustered == i]  
         total_variance += cluster_variance(cluster)
         plt.scatter(cluster[:, 0], cluster[:, 1], color = get_color(i))
-    plt.title('Hierarchical ({} clusters, {} variance)'.format(nb_clusters, total_variance))
+    plt.title('{} ({} clusters, {} variance)'.format(model_name, nb_clusters, total_variance))
     plt.show()
 
-    
-
+for nb_clusters in range(1, 11):
+    cluster_data(nb_clusters, sk.cluster.AgglomerativeClustering, 'Hierarchical')
+    cluster_data(nb_clusters, sk.cluster.KMeans, 'KMeans')
         
 
