46b1378ea026437e1689da58e7a42931d3f15fbf
Vlad Danaila <vlad.danaila@yahoo.com>
2019-10-06 00:18:39 +0300

    Thomson sampling

--- 6 - Reinforcement Learning/33 - Thompson Sampling/thompson_sampling_exercise_1.py
+++ 6 - Reinforcement Learning/33 - Thompson Sampling/thompson_sampling_exercise_1.py
@@ -6,8 +6,22 @@ import scipy.stats
 DATASET_PATH = 'C:/DOC/Workspace/Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/Ads_CTR_Optimisation.csv'
 ads = pd.read_csv(DATASET_PATH).values
 
-for ad in ads:
-    print(ad)
-    
-beta = scipy.stats.beta(a = 10, b = 10)
-beta.pdf([.2, .4, .9])
+def plot():
+    plt.hist(selected)
+    plt.title('Nb. selections ({} iterations)'.format(i + 1))
+    plt.show()
+
+N = len(ads[0])
+rewards = np.ones(10), np.ones(10)
+selected = []
+
+for i in range(len(ads)):
+    beta = np.random.beta(rewards[0], rewards[1], 10)
+    action = np.argmax(beta)    
+    reward = ads[i][action]
+    rewards[reward][action] += 1
+    selected.append(action)
+    if i % 1000 == 0:
+        plot()
+
+plot()
